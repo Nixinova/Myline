7c55e5d86d128655e139b7a306f9bd54fee775d1
Nixinova <nixinova@gmail.com>
2026-09-23 17:58:07 +1200

    Add extra constructor for Timestamp

--- Myline.Core/Models/Timestamp.cs
+++ Myline.Core/Models/Timestamp.cs
@@ -1,3 +1,23 @@
 namespace Myline.Core.Models;
 
-public sealed record Timestamp(DateTime Time, TimestampPrecision Precision);
+public class Timestamp
+{
+	public DateTime Time { get; private init; }
+	public TimestampPrecision Precision { get; private init; }
+
+	public Timestamp(DateTime time, TimestampPrecision precision)
+	{
+		Time = time;
+		Precision = precision;
+	}
+
+	public Timestamp(DateOnly date, TimestampPrecision precision)
+	{
+		if (precision is TimestampPrecision.Hour or TimestampPrecision.Minute or TimestampPrecision.Second)
+		{
+			throw new InvalidOperationException("Invalid timestamp precision");
+		}
+		Time = new DateTime(date.Year, date.Month, date.Day);
+		Precision = precision;
+	}
+}
