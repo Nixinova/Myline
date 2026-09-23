5432cdd50eeb49c29d70279636c70fec66262e15
Nixinova <nixinova@gmail.com>
2026-09-23 21:36:29 +1200

    Also set up cache table

--- Myline.Repository/Database/DataStore.cs
+++ Myline.Repository/Database/DataStore.cs
@@ -8,16 +8,27 @@ public static class DataStore
 		"""
 		create table if not exists History (
 			Id integer primary key autoincrement,
-			Time text not null,
+			Timestamp text not null,
 			Site text not null,
 			Desc text not null
 		);
 
-		create index if not exists idx_History_Time
-		    ON History (Time);
+		create table if not exists Cached (
+			Site text not null,
+			Date text not null
+		);
+
+		create index if not exists idx_History_Timestamp
+		    ON History (Timestamp);
 
 		create index if not exists idx_History_Site
 		    ON History (Site);
+
+		create index if not exists idx_Cached_Site
+		    ON Cached (Site);
+
+		create index if not exists idx_Cached_Date
+		    ON Cached (Date);
 		""";
 
 	public static async Task Init()
